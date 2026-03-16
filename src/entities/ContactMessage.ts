5e50b4bc97f3f58faa43e1ac0b35d52bc83c8840
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-16 19:01:20 +0500

    Add src/entities/ContactMessage.ts

---
+++ src/entities/ContactMessage.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class ContactMessage {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  email!: string;
+
+  @Column('text')
+  message!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
